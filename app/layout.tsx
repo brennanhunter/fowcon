1a2f30456d356bc0f7b64c023b16c5d0450eb433
Hunter Coleman <brennanhunter58@gmail.com>
2026-04-08 13:51:18 -0400

    Fixed removal of globals.css in layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 import SiteChrome from "./components/SiteChrome";
 import { Analytics } from "@vercel/analytics/next";
 
